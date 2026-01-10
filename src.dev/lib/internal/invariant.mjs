925d94dab400788aa3e12664888501656d243c5b
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-10 19:19:14 +0000

    Create internal/invariant.mjs

---
+++ src.dev/lib/internal/invariant.mjs
@@ -0,0 +1,5 @@
+// TODO: (matt) used when -D someting i havent decided is switched on....
+
+export function invariant(condition, message) {
+  if (!condition) throw new Error(`[INTERNAL_INVARIANT]: ${message}`);
+}
