3360715d374c7289b431212c8c835681e3af8ac9
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-14 03:56:59 +0000

    Update disk_fingerprint.mjs

--- src.dev/lib/os/win/disk_fingerprint.mjs
+++ src.dev/lib/os/win/disk_fingerprint.mjs
@@ -9,12 +9,12 @@ import { execSync } from 'node:child_process'
  */
 export function get_disk_fingerprint(drive_letter) {
     // Returns the Volume Serial Number (e.g., "A2C4-DE55")
-    const output = execSync(`wmic volume where "driveletter='${drive_letter}'" get DeviceID, VolumeSerialNumber`).toString()
+    const output= execSync(`wmic volume where "driveletter='${drive_letter}'" get DeviceID, VolumeSerialNumber`).toString()
     
     // TODO(matt): check its valid disk identifier in the windows schema to know the parsing worked properly
     const lines = output.trim().split('\n')
     if (lines.length > 1) {
-        const parts = lines[1].trim().split(/\s+/)
+        const parts= lines[1].trim().split(/\s+/)
         return parts[parts.length - 1]
     }
     
