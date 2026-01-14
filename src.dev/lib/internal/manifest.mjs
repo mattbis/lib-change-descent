3163c84bc3d41c226ad7078d32174a131039282f
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-14 02:27:23 +0000

    Create manifest.mjs

---
+++ src.dev/lib/internal/manifest.mjs
@@ -0,0 +1,4 @@
+/** in persistent storage creates a separate run log.. manifest gathers depending on the mode, a level of detail... every invocation creates a new record 
+ * however, we dont use os logging for this .. thats a version 2 thing... for now its entirely self encapsulated.. and uses the usual location on each os 
+ * for such data, but in one place.
+ */
