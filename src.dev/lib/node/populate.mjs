3472fd921512de7c952e6273330d52707117905d
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-16 16:26:20 +0000

    Update populate.mjs

--- src.dev/lib/node/populate.mjs
+++ src.dev/lib/node/populate.mjs
@@ -1,1 +1,4 @@
-
+/** the session records counts and hashes */
+export function populate_fst_counthash() {}
+/** augment session or trigger with much slower seek */
+export function populate_slw_size() {}
