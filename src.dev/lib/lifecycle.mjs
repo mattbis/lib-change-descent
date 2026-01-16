c81b9a763fd9b48c511dabcbcb9b75d6d4c2c07e
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-16 16:36:57 +0000

    Update lifecycle.mjs

--- src.dev/lib/lifecycle.mjs
+++ src.dev/lib/lifecycle.mjs
@@ -1,7 +1,14 @@
 export function _house_keep() {}
-export function start() {}
+export function start() {
+  return resume()
+}
 export function background() {}
 export function foreground() {}
 export function abort() {}
 export function stop() {}
-export function resume() {}
+export function resume() {
+   // 0. check db integrity 
+   // 0. check fail safe store is aok
+   // 1. house keep interrupted operations
+   // 2. resume buffer load
+}
