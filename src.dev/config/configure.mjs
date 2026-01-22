ac56f1dbb894960898a2d0fee88bf57a59d09a03
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-22 14:58:39 +0000

    Update and rename .keep to configure.mjs

---
+++ src.dev/config/configure.mjs
@@ -0,0 +1,13 @@
+export function configure() {}
+
+export function config_gather() {}
+
+export function _config_defaults() {}
+export function _config_from_env() {}
+export function _config_from_file() {}
+
+export const CONFIG_MOD= {
+    'release': 0,
+    'file': 1,
+    'env': 2
+}
