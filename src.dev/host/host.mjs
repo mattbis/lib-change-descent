d0fc8a73fe37b02a64c150ff4f7b90f2edddd4b4
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-22 14:52:43 +0000

    Update host.mjs

--- src.dev/host/host.mjs
+++ src.dev/host/host.mjs
@@ -1,1 +1,24 @@
 
+/** identify a host runtime, and host machine id when saving session information */
+// TODO (matt): this should call subroutine for each major os line: nix, win, osx
+export function host_fingerprint() {
+    const fingerprint= {
+      
+    }
+    return fingerprint= {}
+}
+
+/** return a load float of system load */
+export function host_metric_load() /* :flot */ {
+    
+}
+
+/** return object containing:-
+timeseries true|false
+metric mem|load|disk_queue
+duration 1s
+interval 500ms
+*/
+export function host_metric(options= {time_series, metric, duration, interval}) {
+    
+}
