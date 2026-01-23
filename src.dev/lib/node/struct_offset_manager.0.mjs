a3d975bb64e5955a94a5c81f7429da3bf10542d3
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-23 13:26:39 +0000

    Update struct_offset_manager.0.mjs

--- src.dev/lib/node/struct_offset_manager.0.mjs
+++ src.dev/lib/node/struct_offset_manager.0.mjs
@@ -11,19 +11,6 @@
  * @property {function(number, number): void} add_flag
  * @property {function(number, number): boolean} has_flag
  */
-
-/**
- * SCHEMA DEFINITION (32 Bytes per Node)
- * 
- * 0: Flags (Uint8) 
- * 1-3: Reserved/Padding
- * 4: ParentId (Int32)
- * 8: NamePointer (Int32)
- * 12: HashPointer (Int32)
- * 16: MTime (Float64)
- * 24: Size (Float64)
- */
-
 export const NODE_STRIDE = 32; // Bytes per node
 
 export const create_node_accessor = (buffer) => {
