faec44658e61dcf96c6864d4a27b8cc01f56cc8e
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-27 23:48:18 +0000

    Update signal.mjs

--- src.dev/lib/signal.mjs
+++ src.dev/lib/signal.mjs
@@ -3,6 +3,8 @@ export signals() {}
 /** gets signals as boolean object */
 export flags() {} 
 /** there isnt one global state this refers to each volume provider and whether it still exists or not, is known or new... etc */
-export cont SIG_STATE = {
-
+export const SIG_STATE = {
+  ABORT: 0,
+  RUN: 1,
+  PROCESS: 2
 }
