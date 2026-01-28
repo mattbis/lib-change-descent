5b7449b56357cada50a7d09d5040c8bc7bcf1a7b
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-28 02:17:45 +0000

    Update configure.mjs

--- src.dev/lib/configure.mjs
+++ src.dev/lib/configure.mjs
@@ -1,1 +1,9 @@
 /** allows programmatic override of defaults - custom locations etc */
+
+/* I dislike environment variables, this library will not use them they are insecure and messy... instead prefer 
+a config file that has *PROPER* permissions... this is much better... */
+export const CONFIG_PRECEDENCE= {
+    file: 0,
+    os: 1,
+    env: 2 
+}
