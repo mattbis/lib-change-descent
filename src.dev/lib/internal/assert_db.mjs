5342a6818051895be4a5129b26bf40b53c9bcd09
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-28 03:14:10 +0000

    Create assert_db.mjs

---
+++ src.dev/lib/internal/assert_db.mjs
@@ -0,0 +1,9 @@
+/** assert db connection */
+
+// initially it uses SQLite
+
+/// assert the db source file... exists
+export function assert_db_file() {}
+
+/// check teh 
+export function assert_db_connection() {}
