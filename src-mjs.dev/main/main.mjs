de61081f8d1f0f66f8581f760d90bc50e10d80ba
Matthew <863968+mattbis@users.noreply.github.com>
2026-06-06 14:08:14 +0100

    Add profile parameter to main function

--- src-mjs.dev/main/main.mjs
+++ src-mjs.dev/main/main.mjs
@@ -1,3 +1,11 @@
 /** expose main functions that allow different ways to consume the library */
-
-export function main() {}
+export function main(profile= 'bg') {
+    switch(profile) {
+        case 'bg':
+            break
+        case 'fg':
+            break
+        default:
+            break
+    }
+}
