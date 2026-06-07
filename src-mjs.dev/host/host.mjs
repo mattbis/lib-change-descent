b375baa62e950419aeb449a88e946b51e3374432
Matthew <863968+mattbis@users.noreply.github.com>
2026-06-07 10:39:36 +0100

    init: boil
    Added host_os function and updated host_fingerprint and host_metric functions.

--- src-mjs.dev/host/host.mjs
+++ src-mjs.dev/host/host.mjs
@@ -1,10 +1,13 @@
+export function host_os() {
+}
 
 /** identify a host runtime, and host machine id when saving session information */
 // TODO (matt): this should call subroutine for each major os line: nix, win, osx
 export function host_fingerprint() {
     const fingerprint= {
+        
     }
-    return fingerprint= {}
+    return fingerprint
 }
 
 /** return a load float of system load */
@@ -20,5 +23,5 @@ duration 1s
 interval 500ms
 */
 export function host_metric(options= {time_series, metric, duration, interval}) {
-    
+        
 }
