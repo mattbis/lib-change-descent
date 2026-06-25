ad1a9b226dfeabb44d2d0f48dcb7f2ace07b462f
Matthew <863968+mattbis@users.noreply.github.com>
2026-06-25 17:25:09 +0100

    Add TODO for LibChangeDescentBare constructor
    Added a TODO comment regarding constructor functionality and framework loading.

--- src-mjs.dev/ctor/libcd_LibChangeDescent.0.mjs
+++ src-mjs.dev/ctor/libcd_LibChangeDescent.0.mjs
@@ -2,6 +2,9 @@
  * expose constructors that allow different ways to consume the library
  */
 
+// TODO: the problem here is that a basic constructor like this needs to support +resident
+// essentially loading a bare framework.. until a connected application instigates actions
+// or with +start ? 
 export class LibChangeDescentBare {
     constructor() {
         
