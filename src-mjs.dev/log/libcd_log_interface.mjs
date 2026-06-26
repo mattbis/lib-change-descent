579892ba2bf8ed9513a0e3737b793ef3fe241005
Matthew <863968+mattbis@users.noreply.github.com>
2026-06-26 17:46:50 +0100

    Create libcd_log_interface.mjs for logging functionality
    Add initial implementation of libcd_log_interface for logging.

---
+++ src-mjs.dev/log/libcd_log_interface.mjs
@@ -0,0 +1,10 @@
+/**
+ * I have my own project that I need to finish for logging that is brutally simple... and very succinct... we will use this 
+ * or you can have it use some other thing, or just console() via new Console() ( this is part of my log library -- as it 
+ * allows output and input streams.... ) TODO (matt): implications of this stream and handles, and corruption etc...
+ * 
+ * since we are session orientated - if something of this severity - doesn't work - it should not be a total deal breaker...
+ * a fallback or automatic name suffix incrementation is a possible solution ( for example if the log file is suddenly corrupted )
+ * as mentioned somewhere else, eventually id implement os level logging using event log in windows, and linux var/log? and osx
+ * who knows, its probably horrific...
+ */
