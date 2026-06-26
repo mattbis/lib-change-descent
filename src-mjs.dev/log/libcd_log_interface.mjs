cf25ffbd20664fbe80a095c19bed8a71a3372f44
Matthew <863968+mattbis@users.noreply.github.com>
2026-06-26 17:49:20 +0100

    Enhance comments on logging strategy and performance
    Added comments discussing logging implementation considerations and performance trade-offs.

--- src-mjs.dev/log/libcd_log_interface.mjs
+++ src-mjs.dev/log/libcd_log_interface.mjs
@@ -7,4 +7,10 @@
  * a fallback or automatic name suffix incrementation is a possible solution ( for example if the log file is suddenly corrupted )
  * as mentioned somewhere else, eventually id implement os level logging using event log in windows, and linux var/log? and osx
  * who knows, its probably horrific...
+ * 
+ * however, having a proxy for logging would be expensive in terms of function context, and engine overhead... and is silly
+ * so whatever solution would need to embed the code as close as possible to runtime bundle, as our aim is the opposite of enterprise 
+ * style stuff.... so its likely it will be chalkpack or console... and later on more flexible ( in a this is going to cost you mode
+ * or probably still pretty expensive - probably solved by bundle messaging of message, instead of real time... ) to os mechanism...
+ * I think here, we need to consult other libraries that are quite low level 
  */
