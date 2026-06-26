c69ef89455c5acb8a3bec41c24ce6f697320f338
Matthew <863968+mattbis@users.noreply.github.com>
2026-06-26 19:40:01 +0100

    Delete src-mjs.dev/lib/libcd_configure.mjs

--- src-mjs.dev/lib/libcd_configure.mjs
+++
@@ -1,9 +0,0 @@
-/** allows programmatic override of defaults - custom locations etc */
-
-/* I dislike environment variables, this library will not use them they are insecure and messy... instead prefer 
-a config file that has *PROPER* permissions... this is much better... */
-export const CONFIG_PRECEDENCE= {
-    file: 0,
-    os: 1,
-    env: 2 
-}
