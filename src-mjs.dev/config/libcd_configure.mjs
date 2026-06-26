e97f1e196860b278ed35e2498f39c215ae3c3b34
Matthew <863968+mattbis@users.noreply.github.com>
2026-06-26 19:38:49 +0100

    Implement configuration management functions and constants
    Added configuration options and constants for managing configuration precedence and export.

--- src-mjs.dev/config/libcd_configure.mjs
+++ src-mjs.dev/config/libcd_configure.mjs
@@ -1,13 +1,38 @@
-export function configure() {}
+/// libcd.configure
 
+export const CONFIG_PRECEDENCE= {
+    'static':0,
+    'command':1,
+    'manifest':2
+}
+
+/** 
+ * options allows programmatic overriding of configuration
+ */
+export function configure(options= {}) {
+    
+}
+
+/** 
+ * synchronous function to execute order of predecence
+ */
 export function config_gather() {}
 
 export function _config_defaults() {}
 export function _config_from_env() {}
 export function _config_from_file() {}
 
+// TODO (matt): os/win/_config_from_registry() {} , .config , 
+// TODO (matt): should all config be actually interchangeable to allow exchange of manifests? probably almost certainly yes ... XML, Yaml, or json, or all of them
+
 export const CONFIG_MOD= {
     'release': 0,
     'file': 1,
     'env': 2
 }
+
+/**
+ * create a config bundle in exchangeable format
+ **/
+export function _config_export(options= {format: 0}) {}
+
