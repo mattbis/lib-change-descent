6797e2211df74161bbde036415ed086521d9a47c
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-03 11:05:14 +0100

    Refactor main function and add _main_switch
    Refactor main function to accept profileMod and add _main_switch function.

--- src-mjs.dev/main/libcd_main.mjs
+++ src-mjs.dev/main/libcd_main.mjs
@@ -1,11 +1,16 @@
 /** expose main functions that allow different ways to consume the library */
-export function main(profile= 'bg') {
-    switch(profile) {
-        case 'bg':
-            break
-        case 'fg':
-            break
-        default:
-            break
-    }
+export funciton main(profileMod) {
+}
+export function _main_switch(profileMod= 'bg') {
+    // TODO (matt): something like
+    profileMod.split(/\+/gi).forEach(m => {
+        switch(profile) {
+            case 'bg':
+                break
+            case 'fg':
+                break
+            default:
+                break
+        }
+    }    
 }
