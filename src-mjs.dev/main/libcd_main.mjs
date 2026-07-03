acdd97e8c8174a0fd160c22e9c69eda7917d036a
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-03 11:05:57 +0100

    Fix typo in export function declaration

--- src-mjs.dev/main/libcd_main.mjs
+++ src-mjs.dev/main/libcd_main.mjs
@@ -1,6 +1,7 @@
 /** expose main functions that allow different ways to consume the library */
-export funciton main(profileMod) {
+export function main(profileMod) {
 }
+
 export function _main_switch(profileMod= 'bg') {
     // TODO (matt): something like
     profileMod.split(/\+/gi).forEach(m => {
