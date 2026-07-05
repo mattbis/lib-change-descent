c058eee7884ab1b5571b48af01b0b89284ff8b76
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-05 23:47:29 +0100

    Enhance comment on library usage and testing
    Expanded comment to clarify usage of the library.

--- src-mjs.dev/lib/vfs/libcd_vfs.mjs
+++ src-mjs.dev/lib/vfs/libcd_vfs.mjs
@@ -1,4 +1,5 @@
 /** 
  * initially used for a fail safe check to ensure, correct permissions and user are able to use the library...
- * otherwise this is pointless
+ * otherwise this is pointless, load using compressed a test file system that mimmicks actions, the output -- db 
+ * can also be pointed to this...
  */
