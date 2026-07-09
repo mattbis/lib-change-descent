4dda099100fb35365249dc8c7832b07d9d003192
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-09 18:50:02 +0100

    Add PROFILE_VOL_PROFILE_CLASS to libcd_profile_classes

---
+++ src-mjs.dev/lib/profile/libcd_profile_classes.mjs
@@ -0,0 +1,5 @@
+export const PROFILE_VOL_PROFILE_CLASS= {
+  // fixed, / mark as ...                   // cannot be fixed and removable
+  // removable, / mark as volatile          // can be removable and temporary
+  // temporary / mark as less important     // can be fixed or removable
+}
