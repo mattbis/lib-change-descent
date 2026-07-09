e3a613bbe67429e3cea4a71153092e87d7e51fa6
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-09 20:30:38 +0100

    Refactor comments and define volume management
    Removed lengthy commentary and added a concise definition for volume management.

--- src-mjs.dev/lib/libcd_volume.mjs
+++ src-mjs.dev/lib/libcd_volume.mjs
@@ -1,23 +1,7 @@
 /**
- * part of the complexity of this library is that there isn't one general set of rules or uses that just work... this stuff can be very brittle
- * particularly when depending on USB interfaces... other connection forms obviously are more robust.
- * only one popular free indexer does this well, but if the disks are missing on startup, which is very easy to happen - perhaps you forgot to 
- * turn on the plug! it will totally rebuild the db.. we never want this - as that is just stupid... I don't know why you would assume all volumes
- * are fixed.. that is a clear - and complete ignorance of world economics, not everyone is as rich as you - or has the latest hardware
- * it's kinda awful and disgusting really... to make such assumptions. Too much software is made by people with overly powerful hardware and no
- * long history in computing since DOS.. since they clearly never once had this thought enter their heads ( I see it all the time with music 
- * software ) ... a clever way to develop and reach the alpha is to test on crap hardware... or deliberately cap CPU. You must think of everyone
- * not just yourself.
- *
- * scale here is important, since this is cost effective, and I'm sure I'm not alone, therefore these strategies are controlled via profile mods at
- * the simplest, and can be user defined if necessary ( i.e., some devices may take a while to spin up - they might be old ) we can't make 
- * assumptions... since until something breaks it could be used as a snapshot or some intermediate: sync, part of a redundant system...
- * 
- * the part missing in the library here is whether to auto assume on magic, if it's some ancient HDD and then probe gently... and hope the OS
- * isn't doing the same thing... TODO (matt): the ultimate interaction uses this as an inverse vector that controls the activity amount... the more
- * batshit insane OS is doing... the less we can do... if it's HDD for example.. as that means head seeking and scratching... we don't want that
- * ever.
+ * define volume management, with sympathy to the __REAL WORLD__ that we are addressing..... 
  */
+
 export const LIBCD_VOL_DISCOVER_STRATEGY = {
     // one by one
     sequential: {
