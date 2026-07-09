8ebf9c07c26ce3ec16575ecc5eebcf385bf2b2ec
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-09 20:26:04 +0100

    Implement base profile classes
    Added base classes for profile management.

--- src-mjs.dev/lib/profile/libcd_profile.mjs
+++ src-mjs.dev/lib/profile/libcd_profile.mjs
@@ -1,1 +1,17 @@
 /// handle profile mods... 
+// each profile class is a mod itself class + class instance...
+
+// this is the slower way to use things////// 
+
+export class Base_Configurable {
+}
+
+export class Configurable extends Base_Configurable {
+}
+
+export class Abstract_Profile {
+}
+
+export class Profile extends Abstract_Profile {
+    
+}
