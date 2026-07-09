d94396c291bd3ca89b9f79a0bae1a7eeea6c84fb
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-09 19:20:52 +0100

    Create libcd_profile.mjs for profile modifications
    Add initial comment for handling profile modifications.

--- src-mjs.dev/lib/profile/libcd_profile_classes.mjs
+++
@@ -1,5 +0,0 @@
-export const PROFILE_VOL_PROFILE_CLASS= {
-  // fixed, / mark as ...                   // cannot be fixed and removable
-  // removable, / mark as volatile          // can be removable and temporary
-  // temporary / mark as less important     // can be fixed or removable
-}
