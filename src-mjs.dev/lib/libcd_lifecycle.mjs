a170b45c38674819913da29be47a944a3a246a9c
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-12 17:05:57 +0100

    Clean up resume function and add self-check comment
    Removed commented-out code from the resume function and added a self-check comment.

--- src-mjs.dev/lib/libcd_lifecycle.mjs
+++ src-mjs.dev/lib/libcd_lifecycle.mjs
@@ -1,3 +1,7 @@
+/**
+ * a combination of mods - creates a executor bundle... however the question arises context, although i don't like it and would prefer classes, that always 
+ * just gets complicated, so for now I'm thinking thisArg.. as simple as possible.
+ */
 export function _house_keep() {}
 export function start() {
   return resume()
@@ -7,8 +11,5 @@ export function foreground() {}
 export function abort() {}
 export function stop() {}
 export function resume() {
-   // 0. check db integrity 
-   // 0. check fail safe store is aok
-   // 1. house keep interrupted operations
-   // 2. resume buffer load
+  // self check
 }
