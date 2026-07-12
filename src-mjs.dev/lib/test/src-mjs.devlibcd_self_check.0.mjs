67082df958cc56d42d5493d4391f9e89519114ed
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-12 17:06:16 +0100

    Enhance self-check with operation comments
    Added comments for self-check operations.

--- src-mjs.dev/lib/test/src-mjs.devlibcd_self_check.0.mjs
+++ src-mjs.dev/lib/test/src-mjs.devlibcd_self_check.0.mjs
@@ -1,3 +1,7 @@
 import { invariant } from "../../lib/internal/libcd_invariant.mjs"
 
 // TODO (matt): the self check is generataed to asceratin if anything is possible.... 
+// 0. check db integrity 
+   // 0. check fail safe store is aok
+   // 1. house keep interrupted operations
+   // 2. resume buffer load
