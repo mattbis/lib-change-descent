7fd6de933e70661dde30d48e239f08c8fa056eec
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-17 02:09:49 +0100

    Rename constants for library configuration

--- src-mjs.dev/config/libcd_constants.mjs
+++ src-mjs.dev/config/libcd_constants.mjs
@@ -1,4 +1,5 @@
 /** the will eventually be schema generated in phase 2 */
 
-export const HEADER_SIZE= 64
-export const MAGIC= "LKMAN001" // the magic will jncrement.. then migration i possible if needed.... 
+export const LIBCD_HEADER_SIZE= 64
+export const LIBCD_MAGIC= 'LKMAN001' // the magic will jncrement.. then migration i possible if needed.... 
+export const LIBCD_CONFIG= ''
