0d6ade56c660c5ad736fb627929a8d3d1c88e5df
Matthew <863968+mattbis@users.noreply.github.com>
2026-07-18 13:13:54 +0100

    Rename classes for consistency and clarity

--- src-mjs.dev/lib/runtime/libcd_LibChangeDescent.0.mjs
+++ src-mjs.dev/lib/runtime/libcd_LibChangeDescent.0.mjs
@@ -5,14 +5,15 @@
 // TODO: the problem here is that a basic constructor like this needs to support +resident
 // essentially loading a bare framework.. until a connected application instigates actions
 // or with +start ? 
-export class libcd_LibChangeDescentBare {
+export class LibCdBare {
     constructor() {
         
     }    
 }
 
 /** the one time DRY might not be sensible - since it would be quite expensive to extend.. however for now */
-export class libcd_LibChangeDescentOp extends libcd_LibChangeDescentBare {
+// run a standalone thing, or custom
+export class LibCdOp extends LibCdBare {
     constructor(options) {
     }
 }
